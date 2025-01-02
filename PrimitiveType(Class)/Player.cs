c2568ad938f8d35127a2a21bfc79fc7cdacd68ee
MD. Famidul Islam Pranto <fipranto9001@gmail.com>
2025-01-02 17:19:43 +0600

    Class

---
+++ PrimitiveType(Class)/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveType_Class_
+{
+    class Player//internal access modifier present
+    {
+        internal string playerName;
+        internal int hp;
+
+        public void Show()
+        {
+            Console.WriteLine(playerName+" "+hp);
+        }
+
+
+    }
+}
--- PrimitiveType(Class)/Program.cs
+++ PrimitiveType(Class)/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace PrimitiveType_Class_
+﻿
+using System;
+using PrimitiveMath;
+
+namespace PrimitiveType_Class_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Calculator cal = new Calculator();
+            var res = cal.Sum(20, 20);
+            Console.WriteLine(res);
+
+            Player player = new Player();
+
+            player.playerName = "FI Pranto";
+            player.hp = 100;
+            player.Show();
         }
     }
 }
