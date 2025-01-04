c8dca769a7a958fdc3f28502da7062c0fc9d66dc
MD. Famidul Islam Pranto <fipranto9001@gmail.com>
2025-01-04 16:12:15 +0600

    Constructor Basic

---
+++ Classes(Constructors)/Enemey.cs
@@ -0,0 +1,30 @@
+﻿namespace Classes_Constructors_
+{
+    class Enemey
+    {
+       public string name;
+       public int hpLevel;
+        List<int> targets;
+
+
+        public Enemey()//ctor is shortcut to create constructor
+        {
+            targets=new List<int>();
+            
+        }
+
+        public Enemey(int hpLevel)
+            :this()
+        {
+            this.hpLevel = hpLevel;
+        }
+        public Enemey(string name,int hpLevel)
+            :this(hpLevel)
+        {
+            this.name = name;
+        }
+
+
+
+    }
+}
---
+++ Classes(Constructors)/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Classes_Constructors_
+{
+    internal class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Enemey drone = new Enemey("Drone",100);
+
+            Console.WriteLine("{0} {1}",drone.name,drone.hpLevel);
+         
+
+
+           
+        }
+    }
+}
