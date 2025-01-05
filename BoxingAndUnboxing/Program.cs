c1715dccd4e111ebb52b04365cc1db4abb8b4191
MD. Famidul Islam Pranto <fipranto9001@gmail.com>
2025-01-05 14:37:57 +0600

    Boxing And Unboxing

---
+++ BoxingAndUnboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace BoxingAndUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boxing
+            Object val = 10; 
+            Console.WriteLine(val);
+
+            //Unboxing
+            int num=(int)val;
+            Console.WriteLine(num);
+
+            ArrayList arrayList = new ArrayList(); //it has a object as argument 
+            //it do boxing when taking adding and unboxing when accessing;
+
+            arrayList.Add(1);
+            arrayList.Add("FI");
+            arrayList.Add(2.5f);
+
+            foreach (var i in arrayList)
+            {
+                Console.WriteLine(i);
+            }
+           
+        }
+    }
+}
