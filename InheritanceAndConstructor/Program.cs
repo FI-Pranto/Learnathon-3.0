88e155651371d79badbd220738ab340e4d524fda
MD. Famidul Islam Pranto <fipranto9001@gmail.com>
2025-01-05 13:01:58 +0600

    Inheritance And Constructor

---
+++ InheritanceAndConstructor/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace InheritanceAndConstructor
+{
+
+    class Player
+    {
+        public string id;
+        public Player(string id)
+        {
+            this.id = id;
+            Console.WriteLine("My Player Id {0}",id);
+        }
+    }
+
+    class Warrior : Player
+    {
+        public Warrior(string id)
+            :base(id)
+        {
+            Console.WriteLine("My Id {0}",id);
+        }
+    }
+    
+        
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior w=new Warrior("W1234");
+        }
+    }
+}
