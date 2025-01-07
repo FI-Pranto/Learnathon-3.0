c78db126fc5386174673fe92b3174c4a0e34cdd0
MD. Famidul Islam Pranto <fipranto9001@gmail.com>
2025-01-07 18:47:20 +0600

    End Of All

---
+++ C#AdvancedTopicsAsyncAwait/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace C_AdvancedTopicsAsyncAwait
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //async await help to maintain the main thread and give ability of parallel processing
+
+            //Must Give this function async Task as a signature 
+            Console.WriteLine("Starting The Application...");
+
+            string result = await DownloadData();
+            Console.WriteLine(result);
+            
+        }
+        public static async Task<string> DownloadData()
+        {
+            Console.WriteLine("Downloading Data....");
+            Thread.Sleep(3000);
+
+            Console.WriteLine("Downloded Successfully...");
+
+            return "Downloaded File";
+        }
+    }
+}
