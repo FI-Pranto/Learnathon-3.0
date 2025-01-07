916d2ddd0860d9b7357202a5d0c464aba146982f
MD. Famidul Islam Pranto <fipranto9001@gmail.com>
2025-01-07 13:22:40 +0600

    Dynamic

---
+++ C#AdvancedTopicsDynamic/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace C_AdvancedTopicsDynamic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //similar to python type changing 
+            dynamic name = "FI Pro";
+            name = 10.5f;//the same variable is converted to float when float assigned
+            Console.WriteLine(name);
+
+            dynamic a = 10;
+            dynamic b = 20;
+            var c = a + b;//because a and b both dynamic c will also become dynamic because of var
+
+            //type conversion
+            int i = 5;
+            dynamic d = i;
+            long l = d;//d is now int and long can hold int so no problem
+
+
+        }
+    }
+}
